8c1596e48afee7a9235fa652c36c9487e750b872
chlorain-0407 <chlorain@gmail.com>
2026-09-18 12:07:42 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ import { generateContentWithFallback, getGenAI } from './server/services/geminiS
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+
 const PORT = process.env.PORT || 3000;
 app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
